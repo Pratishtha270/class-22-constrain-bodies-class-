91e27017093a5f92eda38ace405a851f66554fef
Pratishtha270 <82306312+Pratishtha270@users.noreply.github.com>
2021-08-04 16:15:36 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,84 +1,80 @@
 const Engine = Matter.Engine;
 const World = Matter.World;
 const Bodies = Matter.Bodies;
+const Body = Matter.Body;
+const Constraint = Matter.Constraint;
 
-let engine;
-let world;
-var ball;
-var ball1;
-var ground;
-var con;
-var con1;
-
-
+var con
+var options
+var roof;
+//Create multiple bobs, mulitple ropes varibale here
+var bob1;
+var bob2;
+var bob3;
+var bob4;
+var bob5;
+//creating ropes
+var rope1;
+var rope2;
+var rope3;
+var rope4;
+var rope5;
 
 function setup() {
-  createCanvas(400,400);
-  engine = Engine.create();
-  
-  world = engine.world;
+	createCanvas(800, 600);
+	rectMode(CENTER);
 
-  var ball_options = {
-    restitution: 0.8
-  }
-  
-  
-  ball = Bodies.circle(200,50,10,ball_options);
-  World.add(world,ball);
+	engine = Engine.create();
+	world = engine.world;
 
-  ball1 = Bodies.circle(350,10,12,ball_options);
-  World.add(world,ball1);
+	var roof_options={
+		isStatic:true			
+	}
 
+	roof = Bodies.rectangle(400,100,230,20,roof_options);
+    World.add(world,roof);
+
+	var ball_options = {
+		restitution: 0.4
+		//isStatic:true
+	  }
+
+	bob1 = Bodies.circle(400,300,40,ball_options);
+    World.add(world,bob1);
+
+	bob2 = Bodies.circle(450,300,40,ball_options);
+    World.add(world,bob2);
+
+	bob3 = Bodies.circle(500,300,40,ball_options);
+    World.add(world,bob3);
+
+	bob4 = Bodies.circle(350,300,40,ball_options);
+    World.add(world,bob4);
+
+	bob5 = Bodies.circle(300,300,40,ball_options);
+    World.add(world,bob5);
+
+	rope1=new rope(bob1,roof,-80,0);
+	Engine.run(engine);
+	
   
-  
-  con = Matter.Constraint.create({
-          pointA:{x:200,y:20},
-          bodyB:ball,
-          pointB:{x:0,y:0},
-          length:100,
-          stiffness:0.1
-        });
-  
-      World.add(world,con);
-      
-      con1 = Matter.Constraint.create({
-        pointA:{x:0,y:0},
-        bodyA:ball,
-        bodyB:ball1,
-        pointB:{x:0,y:0},
-        length:100,
-        stiffness:0.1
-      });
-  
-      World.add(world,con1);
-      
-  
+}
+
+function draw() {
   rectMode(CENTER);
-  ellipseMode(RADIUS);
+  background("#99004d");
+
+  rect(roof.position.x,roof.position.y,230,20);
+
+  //call display() to show ropes here
+  rope1.show();
   
+  //create ellipse shape for multiple bobs here
+  ellipse(bob1.position.x,bob1.position.y,40);
+  ellipse(bob2.position.x,bob2.position.y,40);
+  ellipse(bob3.position.x,bob3.position.y,40);
+  ellipse(bob4.position.x,bob4.position.y,40);
+  ellipse(bob5.position.x,bob5.position.y,40);
 }
 
-function draw() 
-{
-  background(51);
-  Engine.update(engine);
-  ellipse(ball.position.x,ball.position.y,10);
-  ellipse(ball1.position.x,ball1.position.y,12);
-
-  push();
-  strokeWeight(2);
-  stroke(255);
-  line(con.pointA.x,con.pointA.y,ball.position.x,ball.position.y);
-  line(ball.position.x,ball.position.y,ball1.position.x,ball1.position.y);
-  pop();
-  
-}
-
-function keyPressed()
-{
-  if(keyCode==RIGHT_ARROW)
-    {
-      Matter.Body.applyForce(ball,{x:0,y:0},{x:0.05,y:0});
-    }
-}
-
+//Write keyPressed function and apply force on pressing up_arrow key on the first bob.
