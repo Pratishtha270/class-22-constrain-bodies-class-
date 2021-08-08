780edd9965a6a5f01b0a12dc0a18be2b00279171
Pratishtha270 <82306312+Pratishtha270@users.noreply.github.com>
2021-08-08 14:31:58 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -39,22 +39,27 @@ function setup() {
 		//isStatic:true
 	  }
 
-	bob1 = Bodies.circle(400,300,40,ball_options);
+	bob1 = Bodies.circle(350,400,40,ball_options);
     World.add(world,bob1);
 
-	bob2 = Bodies.circle(450,300,40,ball_options);
+	bob2 = Bodies.circle(390,400,40,ball_options);
     World.add(world,bob2);
 
-	bob3 = Bodies.circle(500,300,40,ball_options);
+	bob3 = Bodies.circle(430,400,40,ball_options);
     World.add(world,bob3);
 
-	bob4 = Bodies.circle(350,300,40,ball_options);
+	bob4 = Bodies.circle(470,400,40,ball_options);
     World.add(world,bob4);
 
-	bob5 = Bodies.circle(300,300,40,ball_options);
+	bob5 = Bodies.circle(510,400,40,ball_options);
     World.add(world,bob5);
 
-	rope1=new rope(bob1,roof,-80,0);
+	rope1=new Rope(bob1,roof,-80,0);
+	rope2=new Rope(bob2,roof,-40,0);
+	rope3=new Rope(bob3,roof,0,0);
+	rope4=new Rope(bob4,roof,40,0);
+	rope5=new Rope(bob5,roof,80,0);
+
 	Engine.run(engine);
 	
   
@@ -67,7 +72,7 @@ function draw() {
   rect(roof.position.x,roof.position.y,230,20);
 
   //call display() to show ropes here
-  rope1.show();
+ 
   
   //create ellipse shape for multiple bobs here
   ellipse(bob1.position.x,bob1.position.y,40);
@@ -75,6 +80,19 @@ function draw() {
   ellipse(bob3.position.x,bob3.position.y,40);
   ellipse(bob4.position.x,bob4.position.y,40);
   ellipse(bob5.position.x,bob5.position.y,40);
+
+  rope1.display();
+  rope2.display();
+  rope3.display();
+  rope4.display();
+  rope5.display();
 }
 
 //Write keyPressed function and apply force on pressing up_arrow key on the first bob.
+function keyPressed()
+{
+  if(keyCode==UP_ARROW)
+    {
+      Matter.Body.applyForce(bob1,{x:0,y:0},{x:-0.1,y:0});
+    }
+}
